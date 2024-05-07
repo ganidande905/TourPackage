8553d980634d1b5e75179b2805c5868f4f7a559a
ganidande905 <142842955+ganidande905@users.noreply.github.com>
2024-05-07 10:33:39 +0530

    sorted

---
+++ script/gani.js
@@ -0,0 +1,6 @@
+let nexrDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let swipeDom = document.querySelector('.swipe');
+let listItemDom = document.querySelector('.swipe .list');
+let thumbnailDom = document.querySelector('.swipe .thumbnail');
+
