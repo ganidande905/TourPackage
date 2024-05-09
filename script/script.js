bed612472bdeff7610e1a7a3507b4bb7f4332982
ganidande905 <142842955+ganidande905@users.noreply.github.com>
2024-05-09 11:54:30 +0530

    blah

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+let next = document.querySelector('.n')
+let prev = document.querySelector('.p')
 
 next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
+    let items = document.querySelectorAll('.items')
     document.querySelector('.slide').appendChild(items[0])
 })
 
 prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
+    let items = document.querySelectorAll('.items')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
